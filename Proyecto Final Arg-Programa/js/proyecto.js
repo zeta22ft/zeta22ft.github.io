6723527cdb56502ca03b6b82eb0aa2f4a5ccbbf8
zeta22ft <124106302+zeta22ft@users.noreply.github.com>
2023-01-31 21:32:11 -0300

    Add files via upload

---
+++ Proyecto Final Arg-Programa/js/proyecto.js
@@ -0,0 +1,9 @@
+    //alert("El curriculum es ficticio");
+    console.log("No puedo encontrar los errores");
+    console.log("Lo entrego mientras tanto veo cuales pueden llegar a ser los errores");
+
+document.getElementById(`_color`).addEventListener(`click`, function(){
+   document.body.style.backgroundColor = `#f00`; 
+});
+
+
